2174d1de1a19bc82b1720c9a4863cb80938efdcf
jossvagui <joss.vagui@gmail.com>
2022-04-08 23:38:02 -0500

    funcion leer

--- main.js
+++ main.js
@@ -22,7 +22,28 @@ function crear(e){
         libros.push(libro)
         localStorage.setItem("Libros",JSON.stringify(libros))
     }
+    leer();
     document.getElementById("formulario").reset();
     e.preventDefault();
     console.log("Libro guardado correctamente")
 }
+
+//funcion leer cliente
+function leer(){
+    let libros = JSON.parse(localStorage.getItem("Libros"));
+    document.getElementById("tbody").innerHTML = "";
+    for(let i=0; i < libros.length; i++){
+        let titulo = libros[i].titulo
+        let descripcion = libros[i].descripcion
+        let precio = libros[i].precio
+
+        document.getElementById("tbody").innerHTML +=
+        `<tr>
+            <td>${titulo}</td>
+            <td>${descripcion}</td>
+            <td>${precio}</td>
+        </tr>`
+    }
+}
+
+leer();
